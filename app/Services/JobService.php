2a5fa72ed355ada2bf7a05164ed116c66a728aa9
Axel Espinosa <axeldavid45@gmail.com>
2020-02-22 04:15:02 +0000

    Agregamos exceptions a nuestro proyecto

--- app/Services/JobService.php
+++ app/Services/JobService.php
@@ -8,11 +8,9 @@ use App\Models\Job;
 class JobService
 {
     public function deleteJob($id) {
-        //Find the job with id x
-        $job = Job::find($id);
-        if(!$job) {
-            throw new \Exception();
-        }
+        //Find the job with id x or fail and throw and exception
+        $job = Job::findOrFail($id);
+
         //Delete physically from DB
         $job->delete();
     }
